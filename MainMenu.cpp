21fcffc69dbde6f5943d03f5e3f101037dbb9ed7
Ben Stynes(Styner) <45253301+BenStynes@users.noreply.github.com>
2019-01-29 11:04:45 +0000

    Add files via upload

---
+++ MainMenu.cpp
@@ -0,0 +1,167 @@
+#include "MainMenu.h"
+
+
+
+
+
+MainMenu::MainMenu()
+{
+	optionCounterUp = 0;
+	optionCounterDown = 0;
+
+}
+
+void MainMenu::setupText()
+{
+	overallText.loadFromFile("ASSETS/FONTS/PinkBazooka.ttf");
+	titleText.setFont(overallText);
+	titleText.setFillColor(sf::Color::Red);
+	titleText.setCharacterSize(76);
+	titleText.setPosition({ 350.0f, 150.0f });
+	titleText.setString("Suicide Quest");
+	for (int i = 0; i < MAX_OPTIONS; i++)
+	{
+		menuText[i].setFont(overallText);
+		menuText[i].setCharacterSize(43);
+		menuText[i].setFillColor(sf::Color::White);
+		switch (i)
+		{
+		case 0:
+			menuText[i].setPosition({ 400.0f,300.0f });
+			menuText[i].setString("New Game");
+
+			break;
+		case 1:
+			menuText[i].setPosition({ 400.0f,450.0f });
+			menuText[i].setString("How to Play");
+
+			break;
+		case 2:
+			menuText[i].setPosition({ 400.0f,600.0f });
+			menuText[i].setString("Exit Game");
+
+			break;
+		}
+
+
+	}
+
+
+}
+
+void MainMenu::setupOptionsAndPointers()
+{
+
+	optionSelector.setPointCount(3);
+	optionSelector.setFillColor(sf::Color::Blue);
+	optionSelector.setPosition({ 200.0f, 300.0f });
+	optionSelector.setRadius(40);
+}
+
+bool MainMenu::getExisting()
+{
+	return existing;
+}
+
+void MainMenu::setExisting(bool t_showing)
+{
+	existing = t_showing;
+}
+
+void MainMenu::advanceToNewGame()
+{
+	if (optionSelector.getPosition().y == 300.f)
+	{
+		newGame = true;
+		existing = false;
+
+	}
+}
+
+void MainMenu::advanceToOptions()
+{
+
+	if (optionSelector.getPosition().y == 450.f)
+	{
+		optionsMenu = true;
+		existing = false;
+	}
+}
+
+void  MainMenu::exitGame()
+{
+	if (optionSelector.getPosition().y == 600.f)
+	{
+		closeGame = true;
+		existing = false;
+	}
+}
+void MainMenu::movePointer()
+{
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		
+		optionCounterDown++;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+
+		optionCounterDown--;
+	}
+	if (optionCounterDown < 0)
+	{
+		optionCounterDown = 2;
+	}
+	else if (optionCounterDown > 2)
+	{
+		optionCounterDown = 0;
+	}
+	switch (optionCounterDown)
+	{
+	case 0:
+		optionSelector.setPosition(optionSelector.getPosition().x, menuText[optionCounterDown].getPosition().y);
+		break;
+	case 1:
+		optionSelector.setPosition(optionSelector.getPosition().x, menuText[optionCounterDown].getPosition().y);
+		break;
+	case 2:
+		optionSelector.setPosition(optionSelector.getPosition().x, menuText[optionCounterDown].getPosition().y);
+		break;
+	}
+
+}
+void MainMenu::Draw(sf::RenderWindow &t_window)
+{
+	t_window.draw(titleText);
+	t_window.draw(optionSelector);
+	for (int i = 0; i < MAX_OPTIONS; i++)
+	{
+		t_window.draw(menuText[i]);
+		t_window.draw(options[i]);
+	}
+}
+
+bool MainMenu::getNewGame()
+{
+	return newGame;
+}
+
+bool MainMenu::getOptionsMenu()
+{
+	return optionsMenu;
+}
+
+bool MainMenu::getExitGame()
+{
+	return closeGame;
+}
+void MainMenu::returnToMainMenu()
+{
+	if (newGame == false)
+	{
+		existing = true;
+	}
+}
+
---
+++ MainMenu.h
@@ -0,0 +1,46 @@
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+int const MAX_OPTIONS = 3;
+
+class MainMenu
+{
+private:
+	sf::Music menuMusic;
+	sf::Music gameMusic;
+	sf::Music endMusic;
+
+	sf::RectangleShape options[MAX_OPTIONS];
+	sf::Text menuText[MAX_OPTIONS];
+	sf::Text titleText;
+	sf::CircleShape optionSelector;
+	bool existing{ true };
+	bool optionsMenu{ false };
+	bool newGame{ false };
+	bool closeGame{ false };
+	sf::Font overallText;
+	int optionCounterUp;
+	int optionCounterDown;
+
+public:
+	MainMenu();
+	void setupText();
+	void setupOptionsAndPointers();
+	bool getExisting();
+	void setExisting(bool t_showing);
+	void advanceToNewGame();
+	void advanceToOptions();
+	void exitGame();
+	void movePointer();
+	void Draw(sf::RenderWindow & t_window);
+	bool getNewGame();
+	bool getOptionsMenu();
+	bool getExitGame();
+	void returnToMainMenu();
+	/*void setupAudio();
+	void playMenuAudio();
+	void gameplayAudio();*/
+
+};
+
